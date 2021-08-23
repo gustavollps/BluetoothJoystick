e698e4f9c7e13ebd602712dad59da527b4e595f8
gustavollps <gustavollps@gmail.com>
2021-08-23 01:12:38 -0300

    --

--- app/src/main/java/com/example/aircraftcontroller/MainActivity.kt
+++ app/src/main/java/com/example/aircraftcontroller/MainActivity.kt
@@ -15,8 +15,11 @@ import io.reactivex.schedulers.Schedulers
 import com.harrysoft.androidbluetoothserial.SimpleBluetoothDeviceInterface
 import android.content.Context
 import android.util.Log
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
 import io.github.controlwear.virtual.joystick.android.JoystickView
+import java.lang.Exception
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
@@ -32,17 +35,20 @@ class MainActivity : AppCompatActivity() {
         0f,
         0f,
         0f,
-        0f)
+        0f
+    )
 
     private val TAG = "BluetoothMain"
     private var connect: Button? = null
     private var send: Button? = null
+    private var dataReceived: TextView? = null
 
     private val device = "24:6F:28:25:04:22"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         context = this.applicationContext
         bluetoothManager = BluetoothManager.getInstance()
@@ -55,52 +61,54 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
+        dataReceived = findViewById(R.id.receivedData)
+
         connect = findViewById(R.id.connect)
         send = findViewById(R.id.send)
 
-        connect?.setOnClickListener{
+        connect?.setOnClickListener {
             connectDevice(device)
         }
 
-        send?.setOnClickListener{
+        send?.setOnClickListener {
             deviceInterface!!.sendMessage("Hello world!")
         }
 
         throttle_yaw = findViewById(R.id.throttle_yaw)
-        throttle_yaw?.setOnMoveListener{ angle, strength ->
+        throttle_yaw?.setOnMoveListener { angle, strength ->
 
-            joystickData.throttle = (sin(angle * (PI/180f)) * strength).toFloat()
-            joystickData.yaw = (cos(angle * (PI/180f)) * strength).toFloat()
-            Log.i(TAG,"ThrottleYaw: ${joystickData.throttle}, ${joystickData.yaw}")
+            joystickData.throttle = (sin(angle * (PI / 180f)) * strength).toFloat()
+            joystickData.yaw = (cos(angle * (PI / 180f)) * strength).toFloat()
+            Log.i(TAG, "ThrottleYaw: ${joystickData.throttle}, ${joystickData.yaw}")
             Log.i(TAG, "$angle,$strength")
             sendJoystickData()
         }
 
         roll_pitch = findViewById(R.id.roll_pitch)
         roll_pitch?.setOnMoveListener { angle, strength ->
-            joystickData.pitch = (sin(angle * (PI/180f)) * strength).toFloat()
-            joystickData.roll = (cos(angle * (PI/180f)) * strength).toFloat()
+            joystickData.pitch = (sin(angle * (PI / 180f)) * strength).toFloat()
+            joystickData.roll = (cos(angle * (PI / 180f)) * strength).toFloat()
             Log.i(TAG, "RollPitch: ${joystickData.roll}, ${joystickData.pitch}")
             sendJoystickData()
         }
     }
 
-    private fun sendJoystickData(){
+    private fun sendJoystickData() {
         val string: String =
-            "${scaleAndOffset(joystickData.throttle,200f,1000f)}"
-                .padStart(4,'0') +
-            "|${scaleAndOffset(joystickData.yaw,200f,1000f)}"
-                .padStart(4,'0') +
-            "|${scaleAndOffset(joystickData.roll,200f,1000f)}"
-                .padStart(4,'0') +
-            "|${scaleAndOffset(joystickData.pitch,200f,1000f)}"
-                .padStart(4,'0') +
-            "\r\n"
+            "${scaleAndOffset(joystickData.throttle, 200f, 1000f)}"
+                .padStart(4, '0') +
+                    "${scaleAndOffset(joystickData.yaw, 200f, 1000f)}"
+                        .padStart(4, '0') +
+                    "${scaleAndOffset(joystickData.roll, 200f, 1000f)}"
+                        .padStart(4, '0') +
+                    "${scaleAndOffset(joystickData.pitch, 200f, 1000f)}"
+                        .padStart(4, '0') +
+                    "?"
         deviceInterface?.sendMessage(string)
     }
 
     private fun scaleAndOffset(value: Float, range: Float, new_range: Float): Int {
-        return (value * (new_range/range) + new_range/2).toInt()
+        return (value * (new_range / range) + new_range / 2).toInt()
     }
 
     @SuppressLint("CheckResult")
@@ -133,10 +141,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun onMessageReceived(message: String) {
         Log.i(TAG, "Received: $message")
+        dataReceived?.text = message
     }
 
     private fun onError(error: Throwable) {
         // Handle the error
         error.message?.let { Log.e(TAG, it) }
+        try {
+            bluetoothManager?.close()
+        }
+        catch (e: Exception){}
     }
 }
